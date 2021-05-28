a09b636f5bc61861b41e7eece255e1ba55a3bd34
Afrikanochka <Mtgnatali@gmail.com>
2021-05-28 15:34:11 +0300

    add script

---
+++ javascript/test.js
@@ -0,0 +1,7 @@
+const courseTopic = "JavaScript essentials";
+// Change code below this line
+
+const courseTopicLength = courseTopic.lenght;
+const firstElement = courseTopic[0];
+const lastElement = courseTopic.lenght - 1;
+alert(Какое число ?);
